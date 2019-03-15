5052e9fb5754f7278e5fb823117a271ce9e1274d
Tristan Bagot <tristan.bagot@gmail.com>
2019-03-15 10:46:36 +0100

    fix

--- site/controllers/project.php
+++ site/controllers/project.php
@@ -1,15 +1,16 @@
 <?php
 
 return function ($site, $pages, $page) {
-	$title = $page->title()->html();
-	$texts = $pages->visible()->filterBy('intendedTemplate', 'default');
-	$projects = [$page];
+  $title = $page->title()->html();
+  $texts = $pages->visible()->filterBy('intendedTemplate', 'default');
+  $projects = new Collection();
+  $projects->append('0', $page);
 
-	return array(
-	'title' => $title,
-	'texts' => $texts,
-	'projects' => $projects
-	);
+  return array(
+  'title' => $title,
+  'texts' => $texts,
+  'projects' => $projects
+  );
 }
 
 ?>
